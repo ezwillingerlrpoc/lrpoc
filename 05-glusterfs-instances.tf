33fa824588d7ed974b93cea7eca7a2be562b02d6
ezwillingerlrpoc <69393261+ezwillingerlrpoc@users.noreply.github.com>
2020-08-08 10:08:28 -0600

    v0.02 case XXX11
    A lot of progress on the Ansible front, a really annoying problem with the Ansible inventory

--- 05-glusterfs-instances.tf
+++ 05-glusterfs-instances.tf
@@ -2,24 +2,17 @@ resource "aws_network_interface" "storage-cluster-glusterd-net-if" {
     count = local.nb_subnets
     subnet_id = element(local.subnet_ids, count.index)
     description = "Gluster ${count.index} - network interface"
-}
-/*
-data "template_file" "storage-cluster-glusterd-cloudconfig" {
-    #use templatefile instead
-    #not sure if I need anything in here
+    security_groups = [aws_security_group.allow_ssh.id]
 }
 
-data "template_cloudinit_config" "storage-cluster-glusterd-config" {
-    #not sure what to do here
-    #use this object .rendered to use template_file to pass back IPs and whatever
-}
-*/
 resource "aws_instance" "storage-cluster-glusterd-instance" {
     count = local.nb_subnets
     ami = data.aws_ami.rhel_ami.id
     instance_type = var.instance_type
     key_name = aws_key_pair.rsa_ssh_key.key_name
-    user_data = file("userdata.txt")
+    #user_data = file("userdata_cluster.txt")
+    
+
     network_interface {
         network_interface_id = element(aws_network_interface.storage-cluster-glusterd-net-if.*.id, count.index)
         device_index = 0
@@ -30,12 +23,7 @@ resource "aws_instance" "storage-cluster-glusterd-instance" {
         volume_size = var.glusterd_ebs_volume_size
         volume_type = "gp2"
     }
-    ebs_block_device {
-        device_name = "${var.glusterd_ebs_device_name_prefix}g"
-        delete_on_termination = true
-        volume_size = var.glusterd_ebs_volume_size
-        volume_type = "gp2"
-    }
+
 }
 output "ip" {
     value = aws_instance.storage-cluster-glusterd-instance.*.private_ip
