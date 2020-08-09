08ccb19c700ec9fd55d23cffdfe2fefffe6f2b2e
ezwillingerlrpoc <69393261+ezwillingerlrpoc@users.noreply.github.com>
2020-08-09 17:37:27 -0600

    Update LrPocRunMe.ps1
    Updated confirmation text to require Terraform as well

--- LrPocRunMe.ps1
+++ LrPocRunMe.ps1
@@ -1,6 +1,7 @@
 ﻿$confirm = $null
 $confirm = Read-Host -prompt @"
 Please confirm the following:
+0. You have Terraform installed
 1. You have public and private SSH keys (of an acceptable format for AWS EC2 instances) in the paths defined in vars.tf
 2. An IAM user defined with AWS builtin role SystemAdministrator permissions and a corresponding credential file / named profile as defined in vars.tf
 3. You have access to the public internet over TCP/22 from your public IP $((Invoke-WebRequest http://api.ipify.org).content)
